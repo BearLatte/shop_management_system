677fa8b4bb8f853b0ce7094191149f10734820e0
Tim <guoyong19890907@gmail.com>
2023-01-04 05:29:52 +0800

    feat: 实现了登录以及退出的逻辑

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+// 导入全局字体图标
+import './assets/fonts/iconfont.css'
+
+// 导入全局样式表
+import './assets/css/global.css'
+
+// 全局网络请求包
+import axios from 'axios'
+// 配置请求的baseURL
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+// 将 axios 挂载到 vue 的 http 上
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
